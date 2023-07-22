2dcd628ab1a09734c16a106e0d185addd41f0ef8
hereisjohnny2 <carvalho.joaomc@gmail.com>
2023-07-22 17:09:43 -0300

    refactor: update Matrix to use single dim array

--- MicroGrad/Matrix.cs
+++ MicroGrad/Matrix.cs
@@ -6,39 +6,57 @@ public class Matrix
     {
         Rows = rows;
         Cols = cols;
-        Data = new Value[rows, cols];
+        Data = Enumerable.Repeat(new Value(0.0), rows * cols).ToArray();
     }
 
-    public Value[,] Data { get; set; }
+    public Matrix(int rows, int cols, Value[] data)
+    {
+        if (data.Length != rows * cols)
+        {
+            throw new ArgumentException("data length should be equals to rows * cols");
+        }
+
+        Rows = rows;
+        Cols = cols;
+        Data = data;
+    }
+
+    public Value[] Data { get; set; }
 
     public int Cols { get; set; }
 
     public int Rows { get; set; }
 
-    public Value Get(int i, int j)
+    public Value Get(int row, int col)
     {
-        return Data[i, j];
+        return Data[row * Cols + col];
     }
 
-    public void Insert(int i, int j, Value value)
+    public void InsertAt(int row, int col, Value value)
     {
-        Data[i, j] = value;
+        if (row >= Rows || col >= Cols)
+        {
+            throw new ArgumentException("position out of bounds");
+        }
+
+        Data[row * Cols + col] = value;
     }
 
     public Matrix Add(Matrix other)
     {
         if (!Rows.Equals(other.Rows) || !Cols.Equals(other.Cols))
         {
-            throw new ArgumentException("Matrices must have same dimensions");
+            throw new ArgumentException("matrices must have same dimensions");
         }
 
         var output = new Matrix(Rows, Cols);
 
-        for (var i = 0; i < Rows; i++)
+        for (var i = 0; i < Rows; ++i)
         {
-            for (var j = 0; j < Cols; j++)
+            for (var j = 0; j < Cols; ++j)
             {
-                output.Data[i, j] = Data[i, j].Add(other.Data[i, j]);
+                var pos = i * Cols + j;
+                output.Data[pos] = Data[pos].Add(other.Data[pos]);
             }
         }
 
@@ -51,29 +69,30 @@ public class Matrix
         {
             throw new ArgumentException("Matrices must have same dimensions");
         }
-
+    
         var output = new Matrix(Rows, Cols);
-
-        for (var i = 0; i < Rows; i++)
+    
+        for (var i = 0; i < Rows; ++i)
         {
-            for (var j = 0; j < Cols; j++)
+            for (var j = 0; j < Cols; ++j)
             {
-                output.Data[i, j] = Data[i, j].Mul(other.Data[i, j]);
+                var pos = i * Cols + j;
+                output.Data[pos] = Data[pos].Mul(other.Data[pos]);
             }
         }
-
+    
         return output;
     }
-
+    
     public Matrix Dot(Matrix other)
     {
         if (!Cols.Equals(other.Rows))
         {
             throw new ArgumentException("Matrix A number of rows should be equal to Matrix B number of columns");
         }
-
+    
         var output = new Matrix(Rows, other.Cols);
-
+    
         for (var i = 0; i < Rows; i++)
         {
             for (var j = 0; j < other.Cols; j++)
@@ -81,16 +100,17 @@ public class Matrix
                 var elm = new Value(0);
                 for (var k = 0; k < Cols; k++)
                 {
-                    elm = elm.Add(Data[i, k].Mul(other.Get(k, j)));
+                    var pos = i * Cols + k;
+                    elm = elm.Add(Data[pos].Mul(other.Get(k, j)));
                 }
-
-                output.Insert(i, j, elm);
+    
+                output.InsertAt(i, j, elm);
             }
         }
-
+    
         return output;
     }
-
+    
     public override string ToString()
     {
         var output = "[\n";
@@ -99,17 +119,18 @@ public class Matrix
             output += "\t[";
             for (var j = 0; j < Cols; j++)
             {
-                output += $"{Data[i, j].Data:0.0000},";
+                var pos = i * Cols + j;
+                output += $"{Data[pos].Data:0.0000},";
             }
-
+    
             output += "],\n";
         }
-
+    
         output += "]";
-
+    
         return output;
     }
-
+    
     public Value Det()
     {
         
